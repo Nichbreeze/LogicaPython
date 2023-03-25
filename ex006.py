54538af7f5ae8ebac7e23988659424fb37d14104
Nich breeze <nicholas.cavalcante05@gmail.com>
2023-03-25 13:06:13 -0300

    aritimética

---
+++ ex006.py
@@ -0,0 +1,12 @@
+#operador aritmético
+
+import math
+
+
+a = float(input('Digite um number: '))
+
+n1 = a * 2
+n2 = a * 3
+Rq = math.pow(a,1/2)
+
+print('o dobro do seu numero é',n1,'o triplo é',n2,'e a raiz quadrada é',Rq)
